72c1249c1bdc82d682b8229a37e6da3ef12a832d
UdayRajput09 <144793828+UdayRajput09@users.noreply.github.com>
2024-01-16 09:59:32 +0530

    Ebook_App

---
+++ lib/Pages/HomePage/widgets/Appbar.dart
@@ -0,0 +1,43 @@
+import 'package:ebook_app/Pages/ProfilePage/ProfilePage.dart';
+import 'package:ebook_app/config/color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SvgPicture.asset("Assets/Icons/dashboard.svg"),
+        const Text(
+          "E-BOOK",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        InkWell(
+          onTap: () {
+            Get.to(ProfilePage());
+          },
+          child: const CircleAvatar(
+            backgroundColor: Colors.white,
+            child: Text(
+              "K",
+              style: TextStyle(
+                color: fontColor,
+              ),
+            ),
+          ),
+        ),
+        // Text("homepage",)
+      ],
+    );
+  }
+}
